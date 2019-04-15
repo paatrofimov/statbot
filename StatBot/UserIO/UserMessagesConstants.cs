207de0a1117b9526d9502ac81043e14f9fa864c8
patrofimov <patrofimov@skbkontur.ru>
2019-04-15 22:56:09 +0500

    cleanup

--- StatBot/UserIO/UserMessagesConstants.cs
+++ StatBot/UserIO/UserMessagesConstants.cs
@@ -2,19 +2,10 @@
 {
     public static class UserMessagesConstants
     {
-        public const string WaitNextUserInput = "Print next request parameters:";
-
-        public const string AskUserWantsWaitPendingRequests =
-            "Some requests are still processed. Do you want wait until they're done?";
-
         public const string UserConsentOptions = "Yes/No:";
 
         public const string Yes = "Yes";
 
         public const string No = "No";
-
-        public const string PrintRequestProcessStarted = "Request processing started";
-
-        public const string PrintRequestProcessEnded = "Request processing ended";
     }
 }
